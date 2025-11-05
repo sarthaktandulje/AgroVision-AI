49ad287a496efd153857f3fa99645d5b13fff8f9
sarthaktandulje <sarthak.tandulje.portfolio@gmail.com>
2025-11-05 14:49:49 +0530

    Pushing LFS-tracked model and app fixes

--- app.py
+++ app.py
@@ -95,6 +95,6 @@ def predict():
         return jsonify({"error": f"Prediction error: {e}"})
 
 if __name__ == "__main__":
-     import os
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
